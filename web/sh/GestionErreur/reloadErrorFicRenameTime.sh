e78a4c4f44db719124e5c7eec58ee7025e899f5e
Julien <j.desurmont@lci-group.fr>
2019-09-11 10:09:41 +0200

    change reloadErrorFicRenameTime.sh

--- web/sh/GestionErreur/reloadErrorFicRenameTime.sh
+++ web/sh/GestionErreur/reloadErrorFicRenameTime.sh
@@ -3,9 +3,18 @@
 # script qui renomme les fichiers du dossier des fichiers en erreur à rejouer : /fichiers_errors/reloads et les déplace dans le dossiers des fichiers d'origines /fichiers_origines/
 for file in ${BOILERBOX}/web/uploads/tmp/*; do
 	numeroAmodifier=$(echo $file|cut -d '-' -f3|cut -d '.' -f1)
-	nouveauNumero=$(($numeroAmodifier + 1))
+	echo $numeroAmodifier
+	nouveauNumero=$((10#$numeroAmodifier + 1))
+	nb=$(echo $nouveauNumero | wc -c)
+	echo $nb
+	if [ $nb -eq 2 ]
+	then
+		nouveauNumero="0"$nouveauNumero
+	fi
 	echo "Numero " $numeroAmodifier " vs " $nouveauNumero
-	nouveauNom=$(echo $file|sed "s/..\.lci/$nouveauNumero.lci/")
+	echo $file
+	nouveauNom=$(echo $file|sed "s/..\.lci/$nouveauNumero.lci/"|sed "s/\/tmp\//\/tmp\/fichiersModifies\//")
 	echo $nouveauNom
-	#mv $file $nouveaunom
+	mv $file $nouveauNom
 done
+
