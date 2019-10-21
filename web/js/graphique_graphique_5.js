1e0f400932f22fc57254ff4b0415e48d1d7b4f57
Julien <j.desurmont@lci-group.fr>
2019-10-21 12:03:41 +0200

    V 2.9.0

--- web/js/graphique_graphique_5.js
+++
@@ -1,3 +0,0 @@
-;$('#choixSelectionPeriode').hide();$(document).ready(function(){closePopupCompression();$('configperiode').off('click');$(document).on('keypress',function(e){if((e.which||e.keyCode)===105){if($('#infoTooltip').prop('checked')==!0){$('#infoTooltip').prop('checked',!1)}
-else{$('#infoTooltip').prop('checked',!0)};changeTooltip()}})});function imprimeCsv(e,o){if($('#impression').val()!='no'){switch(e){case'yesCsv':$('#impression').val('yesCsv');break;case'yesPCsv':break};submitForm(o);$('#impression').val('no')}
-else{return 1};return 0};
